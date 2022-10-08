4df92e7f8ee5cefa29b75e372634bd7cfe542544
AnnaTsishk <110522831+AnnaTsishk@users.noreply.github.com>
2022-10-08 11:54:45 +0300

    Readme2.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+    }
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
-}
+    // <div className="App">
+     {/*Hello, samurai! Let's go!*/}
 
 export default App;
